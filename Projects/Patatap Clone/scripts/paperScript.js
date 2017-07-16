655c5fbb0e57db3470e92ade2ad42a4127dc8759
mechae <mehdi.chaid@polymtl.ca>
2017-07-16 19:37:12 -0400

    Patatap Clone Finished

--- Projects/Patatap Clone/scripts/paperScript.js
+++ Projects/Patatap Clone/scripts/paperScript.js
@@ -1,26 +1,23 @@
-// var dataSet = {
-//     a: {
-//         color: "orange"
-//     },
-//     q: {
-//         color: "blue"
-//     },
-
-//     e: {
-//         color: "green"
-//     },
-
-//     f: {
-//         color: "red"
-//     }
-// }
+var circlesArray = [];
 
 
 function onKeyDown(event) {
-    var maxPoint = new Point(view.size.width, view.size.height);
-    var randomPoint = Point.random();
-    var point = maxPoint * randomPoint;
-    // console.log(dataSet[event.key.color]);
-    // new Path.Circle(point, 50).fillColor = dataSet[event.key.color];
-    new Path.Circle(point, 50).fillColor = "blue";
+    if (dataSet[event.key]) {
+        var maxPoint = new Point(view.size.width, view.size.height);
+        var randomPoint = Point.random();
+        var point = maxPoint * randomPoint;
+        var circle = new Path.Circle(point, 100);
+        circle.fillColor = dataSet[event.key]["color"];
+        circlesArray.push(circle);
+        dataSet[event.key]["sound"].play();
+    }
+}
+
+function onFrame(event) {
+    circlesArray.forEach(function (circle) {
+        circle.fillColor.hue += 1;
+        circle.position.x += 1;
+        circle.position.y += 1;
+        circle.scale(0.97);
+    });
 }
